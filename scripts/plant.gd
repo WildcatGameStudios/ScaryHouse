86bb45dec78afc48d86539554ed5807671c60daf
EliasNajjar <eliasnajjar111@gmail.com>
2025-09-21 00:58:01 -0400

    more code for plants

--- scripts/plant.gd
+++ scripts/plant.gd
@@ -3,25 +3,37 @@ extends Node3D
 @onready var csg_box_3d: CSGBox3D = $CSGBox3D
 
 @export var time_to_activate: float = 0 # the plant will not start dying for this amount of time
+var activate_timer: float = time_to_activate
 @export var time_between_dying_rolls: float = 5 # while active, time between rolls to start dying
 var dying_rolls_timer: float = time_between_dying_rolls
-@export var start_dying_chance: float = .5 # chance to start dying every time_between_chance_rolls
+@export var start_dying_chance: float = .1 # chance to start dying every time_between_chance_rolls
 @export var time_to_die: float = 30 # amount of time until the plant dies after being activated
 var death_timer: float = time_to_die
 
-func _process(delta: float) -> void:
-	if time_to_activate > 0: # if not active
-		time_to_activate -= delta
-	elif dying_rolls_timer > 0:
-		dying_rolls_timer -= delta
-	elif death_timer == time_to_die:
-		if randf() < start_dying_chance:
-			
+func inactive(delta: float) -> void:
+	activate_timer -= delta
+
+func exit_inactive() -> void:
+	pass
+
+func enter_active() -> void:
+	pass
+
+func active(delta: float) -> void:
+	dying_rolls_timer -= delta
+
+func exit_active() -> void:
+	dying_rolls_timer = time_between_dying_rolls
+
+func enter_dying() -> void:
+	pass
+
+func dying(delta: float) -> void:
+	death_timer -= delta
+	if death_timer > time_to_die / 2:
+		csg_box_3d.material.albedo_color = Color(2 * (1 - death_timer / time_to_die),1,0)
 	else:
-		death_timer -= delta
-		if death_timer <= 0:
-			pass
-		if death_timer > time_to_die / 2:
-			csg_box_3d.material.albedo_color = Color(2 * (1 - death_timer / time_to_die),1,0)
-		else:
-			csg_box_3d.material.albedo_color = Color(1,2 * death_timer / (time_to_die),0)
+		csg_box_3d.material.albedo_color = Color(1,2 * death_timer / (time_to_die),0)
+
+func exit_dying() -> void:
+	death_timer = time_to_die
--- scripts/plant_sm.gd
+++ scripts/plant_sm.gd
@@ -17,33 +17,48 @@ func _ready() -> void:
 
 # Check if there is a state to execute 
 func _physics_process(delta):
+	get_transition(delta)
 	if state != null : 
 		state_logic(delta)
-	
 
 func state_logic(delta) : 
 	match state:
 		"inactive":
-			parent.inactive()
+			parent.inactive(delta)
 		"active":
-			parent.active()
+			parent.active(delta)
 		"dying":
-			parent.dying()
+			parent.dying(delta)
 	
 func get_transition(delta):
-	return null
-	
-func enter_state(new_state, old_state) : 
-	match new_state:
+	match state:
 		"inactive":
-			parent.enter_inactive()
+			if parent.activate_timer <= 0:
+				set_state("active")
+		"active":
+			if parent.dying_rolls_timer <= 0:	
+				if randf() < parent.start_dying_chance:
+					set_state("dying")
+				else:
+					parent.dying_rolls_timer = parent.time_between_dying_rolls
+		"dying":
+			pass
+	
+func enter_state(new_state, old_state) :
+	match state:
 		"active":
 			parent.enter_active()
 		"dying":
 			parent.enter_dying()
 	
 func exit_state(old_state, new_state) : 
-	pass
+	match new_state:
+		"inactive":
+			parent.exit_inactive()
+		"active":
+			parent.exit_active()
+		"dying":
+			parent.exit_dying()
 
 
 func set_state(new_state) : 
