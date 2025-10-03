c928b7e97d2aca272d3dd91b88c35ea9a81d597c
EliasNajjar <eliasnajjar111@gmail.com>
2025-10-02 23:25:08 -0400

    made editor position plugin more user friendly

--- addons/set_editor_position/set_editor_position.gd
+++ addons/set_editor_position/set_editor_position.gd
@@ -10,26 +10,22 @@ var go_button: Button
 func _enter_tree():
 	hbox = HBoxContainer.new() # to add to toolbar all at once
 	
-	# x input
-	x_input = LineEdit.new()
-	x_input.placeholder_text = "X=0" # default value is 0
+	x_input = LineEdit.new() # x input
+	x_input.placeholder_text = "x=0" # default value is 0
 	x_input.size_flags_horizontal = Control.SIZE_EXPAND_FILL # scroll for long input
 	hbox.add_child(x_input) # add to hbox
 	
-	# y input
-	y_input = LineEdit.new()
-	y_input.placeholder_text = "Y=0"
+	y_input = LineEdit.new() # y input
+	y_input.placeholder_text = "y=0"
 	y_input.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	hbox.add_child(y_input)
 	
-	# z input
-	z_input = LineEdit.new()
-	z_input.placeholder_text = "Z=0"
+	z_input = LineEdit.new() # z input
+	z_input.placeholder_text = "z=0"
 	z_input.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	hbox.add_child(z_input)
 	
-	# go button
-	go_button = Button.new()
+	go_button = Button.new() # go button
 	go_button.text = "Go"
 	go_button.pressed.connect(_on_go_pressed) # call _on_go_pressed when pressed
 	hbox.add_child(go_button)
@@ -41,13 +37,4 @@ func _exit_tree():
 	hbox.queue_free()
 	
 func _on_go_pressed():
-	var x = x_input.text.to_float() # convert inputs to numbers, .to_float() deals with string to float conversion very well
-	var y = y_input.text.to_float()
-	var z = z_input.text.to_float()
-	var pos = Vector3(x,y,z) # create vector from inputs
-	var cam = EditorInterface.get_editor_viewport_3d().get_camera_3d() # get reference to editor camera
-	
-	cam.global_position = pos # set position to pos, however this sets a central pivot only applied after some time
-	await get_tree().create_timer(.5).timeout # pause to let pevious transport happen, you will end up looking at the point you want
-	cam.global_position += 2 * (pos - cam.global_position) # set position to opposite side but same distance from pivot, this makes the camera end up on the old pivot
-	# see details about why the camera is set this way in the README
+	EditorInterface.get_editor_viewport_3d().get_camera_3d().global_position = Vector3(x_input.text.to_float(),y_input.text.to_float(),z_input.text.to_float())
