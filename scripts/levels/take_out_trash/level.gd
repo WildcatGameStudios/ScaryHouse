57b35c61f4b50c862f7ae570145bc8564eea56c5
NevinMyers <nevinmyers@gmail.com>
2025-10-14 20:28:49 -0400

    take out trash score evaluator

--- scripts/levels/take_out_trash/level.gd
+++ scripts/levels/take_out_trash/level.gd
@@ -5,21 +5,27 @@ extends Node3D
 @export var base_trash_launch_strength: Vector2 = Vector2(30,5)
 #trash bin movement speed increase on binned trash
 @export var bin_speed_increase: float = 0.2
+@export var total_trash: int
 
 #holds the trash that is in the catapult
 var current_trash: Node3D
+#number of bags thrown
+var thrown_counter: int = 0
 #number of bags binned
-var binned_counter: int
+var binned_counter: float = 0
 
 func launch_trash(throw_strength: float):
 	#exit if no trash to launch
 	if current_trash == null:
 		return
+	
 	#add velocity rotated to catapult arm rotation
 	current_trash.velocity = throw_strength * Vector3(base_trash_launch_strength.x,base_trash_launch_strength.y,0).rotated(Vector3(0,1,0),catapult.catapult_arm.rotation.y-PI/2)
 	#empty current trash because it's not longer in the catapult
 	current_trash = null
-	$TrashCooldown.start()
+	thrown_counter += 1
+	if thrown_counter < total_trash:
+		$TrashCooldown.start()
 
 func load_trash():
 	#exit if there is trash loaded
@@ -45,12 +51,16 @@ func trash_collided(hit_bin: bool):
 	if hit_bin:
 		print("hit")
 		binned_counter += 1
-		#bins start moving when 4 trash is binned
-		if binned_counter >= 4:
+		#bins start moving when 3 trash is binned
+		if binned_counter >= 3:
 			$AnimationPlayer.play("slide_bins")
 			$AnimationPlayer.speed_scale += bin_speed_increase
 	else:
 		print("no hit")
+
+func evalutate_score() -> float:
+	var score = 100 * (binned_counter / total_trash)
+	return score
 
 func _ready() -> void:
 	catapult.throw_trash.connect(launch_trash)
@@ -61,6 +71,7 @@ func _physics_process(delta: float) -> void:
 		current_trash.global_position = catapult.catapult_bowl.global_position + Vector3(0,3,0)
 		#remove velocity from gravity
 		current_trash.velocity = Vector3.ZERO
+	print(evalutate_score())
 
 #replace trash after launch and short cooldown
 func _on_trash_cooldown_timeout() -> void:
